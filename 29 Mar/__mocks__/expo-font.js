f1c8667c7d01898e547c386206cb7be598e62447
Kevin Lim <56734553+kevinlim512@users.noreply.github.com>
2025-03-29 08:37:05 +0800

    Delete 29 Mar directory

--- 29 Mar/__mocks__/expo-font.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    loadAsync: jest.fn(() => Promise.resolve()),
-    isLoaded: jest.fn(() => true),
-    isLoading: jest.fn(() => false),
-    unloadAsync: jest.fn(() => Promise.resolve()),
-};
--- 29 Mar/index.js
+++
@@ -1,5 +0,0 @@
-import 'react-native-reanimated';
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-registerRootComponent(App);
